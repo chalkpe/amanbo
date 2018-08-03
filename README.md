716c52d85cef8aca5085d112fc6184d9c09483ae
Chalk <chalk@chalk.pe>
2018-08-04 03:51:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# amanbo
+[Hearthstone](https://playhearthstone.com/ko-kr) quiz page
+
+## Usage
+```bash
+git clone https://github.com/ChalkPE/amanbo
+cd amanbo && yarn install && yarn test && yarn build
+```
+
+## License
+[MIT](LICENSE)
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div id="app">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return { name: 'wow!' }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    background-color: red;
+  }
+</style>
